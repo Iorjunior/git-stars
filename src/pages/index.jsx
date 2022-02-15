ac94d60ffc65f282600a6d789e51cf04c9754745
iorjunior <juniorcoda@gmail.com>
2022-02-15 17:29:03 -0300

    feat: initial config project
    config struct paste, config tailwindcss and commitizen

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+    return (
+        <div className="">
+            <Head>
+                <title>Git Stars</title>
+                <meta name="description" content="" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+        </div>
+    );
+}
